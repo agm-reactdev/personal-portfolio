1478e6601927bfb2a9b938e73c1bacff195476ba
agm-reactdev <abdulghaffarmemon052@gmail.com>
2026-08-16 09:34:58 +0500

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ function App() {
   return (
     <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! kese ho m thk hun ap batao
     </h1>
+
     </>
   )
 }
