42b8be68de91e19e120ff674a6b47b90c3cb73c6
agm-reactdev <abdulghaffarmemon052@gmail.com>
2026-08-16 12:12:33 +0500

    Added Navbar and Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,33 @@
 function Footer () {
     return (
-        <div>
-            <p className=" text-3xl text-center underline">@Disclaimer dont copy my code !</p>
+        <div className="flex flex-col  bg-gray-700 text-white px-8 py-4">
+            <div className="flex justify-between gap-4 ">
+                <div>
+                    <h2> <a href="#">Abdul Ghaffar</a> </h2>
+                    <p>
+                        Junior Frontend Web Developer crafting clean,<br /> 
+                        responsive, and user-friendly web interfaces.
+                    </p>
+                </div>
+                <div>
+                    
+                    <ul className="flex flex-col gap-4">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p>Github : agm_dev</p>
+                    <p>Linkdin : abdulghaffar</p>
+                    <p>Email : abdulghaffarmemon052@example.com</p>
+                </div>
+            </div>
+
+            <div className="mt-4">
+                <p className="text-center">© 2023 Abdul Ghaffar. All rights reserved.</p>
+            </div>
         </div>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,23 @@
 function Navbar() {
    return (
-     <div>
-        <h1 className="text-2xl font-bold">My Portfolio</h1>
+     <div className="flex justify-between items-center h-24 px-8 bg-gray-800 text-white">
+         <div>
+            <h1 className="text-2xl font-bold"><a href="#">Abdul Ghaffar</a></h1>
+         </div>
+          
+          <div>
+             <ul className="flex gap-8 ">
+               <li>Home</li>
+               <li>About</li>
+               <li>Portfolio</li>
+               <li>Contact</li>
+             </ul>
+          </div>
+
+          <div className="flex gap-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Resume</button>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Contact Me</button>
+          </div>
      </div>
    )
 }
